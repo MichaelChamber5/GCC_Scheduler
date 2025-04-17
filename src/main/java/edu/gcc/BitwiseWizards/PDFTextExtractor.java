8a331c005dab35d0ae758e5e2f052b07e7f85225
Aiden <aidenvangura@gmail.com>
2025-04-17 16:27:41 -0400

    fixes to handleing and formatting

--- src/main/java/edu/gcc/BitwiseWizards/PDFTextExtractor.java
+++ src/main/java/edu/gcc/BitwiseWizards/PDFTextExtractor.java
@@ -9,9 +9,16 @@ import java.io.IOException;
 public class PDFTextExtractor {
     public static String extractText(String pdfPath) throws IOException {
         try (PDDocument document = PDDocument.load(new File(pdfPath))) {
+            if (document.isEncrypted()) {
+                throw new IOException("PDF is encrypted and cannot be processed");
+            }
+            
             PDFTextStripper stripper = new PDFTextStripper();
+            stripper.setSortByPosition(true);
             return stripper.getText(document);
+        } catch (IOException e) {
+            System.err.println("Error extracting text from PDF: " + e.getMessage());
+            throw e;
         }
     }
-
 }
